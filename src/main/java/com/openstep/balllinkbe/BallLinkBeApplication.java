96668391e350d7ed13edc5c27a292ac624cd0f4c
이태현 <jade@egiskorea.com>
2025-09-04 13:36:58 +0900

    feat: 프로젝트 초기 설정
    의존성, ignore, DB설정 추가

---
+++ src/main/java/com/openstep/balllinkbe/BallLinkBeApplication.java
@@ -0,0 +1,13 @@
+package com.openstep.balllinkbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallLinkBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BallLinkBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openstep/balllinkbe/BallLinkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openstep.balllinkbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallLinkBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
